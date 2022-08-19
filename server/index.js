ef4ba15961ebf7e90a2875cca180956590ab0ff8
JKAdministrator <julio.kania@gmail.com>
2022-08-19 18:00:10 -0300

    finished api + client

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const colors = require("colors");
+const cors = require("cors");
 const express = require("express");
 const { graphQLHTTP, graphqlHTTP } = require("express-graphql");
 const PORT = process.env.PORT || 5000;
 const app = express();
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
+
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
